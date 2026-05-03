d5d4e40aad102a6b1ce965b1e16f48f5bce3e97d
init0-lux <init0.lux@protonmail.com>
2026-05-03 12:15:59 +0530

    feat: contact model based on VCF 4.0;

---
+++ internal/model/contact.go
@@ -0,0 +1,56 @@
+package model
+
+type SourceInfo struct {
+	File string
+	Row  int
+}
+
+type Contact struct {
+	ID           string
+	Name         string
+	Phones       []string
+	Emails       []string
+	Organization string
+	Metadata     map[string]string
+	Source       SourceInfo
+}
+
+type NormalizedName struct {
+	FullName     string
+	FirstName    string
+	LastName     string
+	Organization string
+	Normalized   bool
+}
+
+type NormalizedPhone struct {
+	Raw        string
+	Normalized string
+	Valid      bool
+	Country    string
+}
+
+type MergedContact struct {
+	Contact
+	MergedFrom  []string
+	Confidence  float64
+	MergeReason []string
+}
+
+type DedupeReport struct {
+	TotalInput      int
+	DuplicatesFound int
+	ClustersFormed  int
+}
+
+type DedupeResult struct {
+	Clusters [][]Contact
+	Merged   []MergedContact
+	Report   DedupeReport
+}
+
+type ParseError struct {
+	Row     int
+	Field   string
+	Message string
+}
